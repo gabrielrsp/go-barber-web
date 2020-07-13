a627c8bf983845d0aaee2c5170b85ca651c55f71
gabrielrsp <gabrielrsp18@gmail.com>
2020-07-13 00:41:17 -0300

    animate toasts

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -7,6 +7,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -15,7 +16,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 }
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -30,7 +31,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [message.id, removeToast]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description} >
+    <Container
+      type={message.type}
+      hasDescription={!!message.description}
+      style={style}
+    >
       {icons[message.type || 'info']}
       <div>
         <strong>{message.title}</strong>
--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
+
 
 import Toast from './Toast';
 
@@ -10,12 +12,24 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+    );
+
   return (
     <Container>
-      {messages.map(message => (
+      {messagesWithTransitions.map(({ item, key, props }) => (
         <Toast
-          key={message.id}
-          message={message}
+          key={key}
+          style={props}
+          message={item}
         />
       ))}
     </Container>
