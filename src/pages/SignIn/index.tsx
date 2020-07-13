b639e4b786178fd7480d3d3f5bfbc34bc3114b25
gabrielrsp <gabrielrsp18@gmail.com>
2020-07-13 20:42:48 -0300

    implement routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,8 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -13,7 +15,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -24,8 +26,8 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
-
   const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -45,12 +47,15 @@ const SignIn: React.FC = () => {
         password: data.password,
       });
 
-    } catch (err) {
+      history.push('/dashboard');
 
+    } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err)
 
         formRef.current?.setErrors(errors);
+
+        return;
       }
       addToast({
         type: 'error',
@@ -58,31 +63,32 @@ const SignIn: React.FC = () => {
         description: 'Something went wrong, please check your credentials',
       });
     }
-  }, [signIn, addToast]);
+  }, [signIn, history, addToast]);
 
   return (
 
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Login</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Login</h1>
 
-          <Input name="email" icon={FiMail} placeholder="Email" />
-          <Input name="password" icon={FiLock} type="password" placeholder="Password" />
+            <Input name="email" icon={FiMail} placeholder="Email" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Password" />
 
-          <Button type="submit">Enter</Button>
+            <Button type="submit">Enter</Button>
 
-          <a href="forgot">I Forgot my password</a>
-        </Form>
+            <a href="forgot">I Forgot my password</a>
+          </Form>
 
-        <a href="create">
-          <FiLogIn />
+          <Link to="/signup">
+            <FiLogIn />
         Create account
-      </a>
+      </Link>
+        </AnimationContainer>
       </Content>
-
       <Background />
     </Container>
 
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,18 +3,31 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import getValidationErrors from '../../utils/getValidationErrors';
+import { Link, useHistory } from 'react-router-dom';
 
+
+import { useToast } from '../../hooks/toast';
+import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
+
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -28,34 +41,56 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-    } catch (err) {
-      const errors = getValidationErrors(err)
+      await api.post('/users', data);
 
-      formRef.current?.setErrors(errors);
+      history.push('/');
+
+      addToast({
+        type: 'success',
+        title: 'You are registered!',
+        description: 'You can login now'
+      });
+
+
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err)
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+      addToast({
+        type: 'error',
+        title: 'Failed to sign up',
+        description: 'Something went wrong while registering. try again',
+      });
     }
-  }, []);
+  }, [addToast, history]);
 
   return (
 
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit} >
-          <h1>Create your account</h1>
+          <Form ref={formRef} onSubmit={handleSubmit} >
+            <h1>Create your account</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Name" />
-          <Input name="email" icon={FiMail} placeholder="Email" />
-          <Input name="password" icon={FiLock} type="password" placeholder="Password" />
-          <Button type="submit">Create</Button>
+            <Input name="name" icon={FiUser} placeholder="Name" />
+            <Input name="email" icon={FiMail} placeholder="Email" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Password" />
+            <Button type="submit">Create</Button>
 
-        </Form>
+          </Form>
 
-        <a href="login">
-          <FiArrowLeft />
+          <Link to="/">
+            <FiArrowLeft />
         Back to Login
-      </a>
+      </Link>
+        </AnimationContainer>
       </Content>
     </Container>
 
